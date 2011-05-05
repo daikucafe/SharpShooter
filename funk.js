7a48d25cc0a47e9f61dead3025ec7a60a6bf5f75
Marco Torres Morgado <ouji@koori-code.net>
2011-05-05 13:17:27 -0300

    Arreglado js. Readme

--- funk.js
+++ funk.js
@@ -1,24 +1,11 @@
-function nave = {};
-function prop = {};
-function enemy = {};
-
-window.onLoad(initGame());
-
-var w;
-var h;
+function nave(){}
+function prop(){}
+function enemy(){}
 
 function initGame(){
 	var canvas = document.getElementById("screen");
 	var g = canvas.getContext("2d");
 	
-	w = canvas.width;
-	h = canvas.height;
-	
-	g.fillStyle = "rgb(30,30,30)";
-	g.fillRect(0, 0, canvas.width, canvas.height);
+	g.fillStyle="rgb(30,30,30)";
+	g.fillRect(0,0,canvas.width,canvas.height);
 }
-
-function step(){
-}
-
-
